9d00216ee39fcc98d3f0e9daa618e0c61ae79171
rudi8848 <anna.v8848@gmail.com>
2018-08-12 14:11:13 +0300

    cbreak mode terminal

--- cursor.c
+++ cursor.c
@@ -1,19 +1,22 @@
 #include "21sh.h"
 #include <ctype.h>
+#include <strings.h>
 
 # define K_LEFT		4479771
 # define K_RIGHT	4414235
 # define K_UP		4283163
 # define K_DOWN		4348699
 
-# define K_ENTER	13
-//# define K_ENTER	10
+//# define K_ENTER	13
+# define K_ENTER	10
 # define K_TAB		9
 # define K_SPACE 	32
 # define K_ESC		27
 # define K_DELETE	2117294875L
 # define K_BSPACE	127
 
+
+#define MAXLINE 500
 
 struct termios saved;
 struct termios changed;
@@ -42,7 +45,7 @@ void			ft_set_signals(void)
 	//signal(SIGWINCH, ft_winch_handler);
 }
 
-int 	change_settings(void)
+int 	raw_settings(void)
 {
 	changed = saved;
 
@@ -56,42 +59,92 @@ int 	change_settings(void)
 	changed.c_cc[VTIME] = 0;
 
 
-	int ret = tcsetattr(STDIN_FILENO, TCSAFLUSH, &changed);
-	if (ret == -1)
+	//int ret = tcsetattr(STDIN_FILENO, TCSAFLUSH, &changed);
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &changed) == -1)
 	{
 		perror(__FUNCTION__);
-		exit(0);
+		ft_exit();
 	}
+	return 0;
+}
+
+int 	cbreak_settings()
+{
+	changed = saved;
+
+	changed.c_lflag &= ~(ICANON | ECHO);
+	changed.c_cc[VMIN] = 1;
+	changed.c_cc[VTIME] = 0;
+
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &changed) == -1)
+	{
+		perror(__FUNCTION__);
+		ft_exit();
+	}
+
 	return 0;
 }
 
 int		read_loop(void)
 {
+	char line[MAXLINE];
+
+
 int			rr;
 	uint64_t	rb;
 	//t_dslist	*ptr;
 
 	//ptr = g_attr.args;
+	bzero(line, MAXLINE);
 	rb = 0;
+	int len = 0;
+	int i = 0;
 	//ft_print_forward(ptr);
 	while ((rr = read(STDIN_FILENO, &rb, 8)) > 0)
 	{
-		if (rb == K_RIGHT || rb == K_LEFT)
-			//ft_move_h(&ptr, rb);
-			printf("LEFT RIGHT\n");
+		if (rb == K_RIGHT)
+		{
+			i++;
+		}
+		else if (rb == K_LEFT )
+		{
+			i--;
+		}
+		else if (rb == K_ENTER)
+			{//return (ft_print_selected(g_attr.args));
+				len++;
+				printf("\n");
+				//передавать в лексер сo '\n'???
+				if (len)
+				{
+					printf("[GOT:] %s [len = %d, index = %d]\n", line, len, i);
+					bzero(line, len);
+					i = 0;
+					len = 0;
+				}
+				
+			}
 		else if (isprint(rb))
+		{
 			write(STDOUT_FILENO, &rb, rr);
+			line[i] = (char)rb;
+			i++;
+			len++;
+		}
 		else if (rb == K_DOWN || rb == K_UP)
 			//ft_move_v(&ptr, rb);
 			printf("UP DOWN\n");
 		//else if (rb == K_SPACE)
 		//	ft_switch_mode(ptr, M_SLCT);
-		else if (rb == K_DELETE || rb == K_BSPACE)
-		//	ft_delete_active(&ptr);
-			printf("DELETE\n");
-		else if (rb == K_ENTER)
-			//return (ft_print_selected(g_attr.args));
-			printf("ENTER\n");
+		else if (rb == K_DELETE)
+		{
+
+		}
+		else if (rb == K_BSPACE)
+		{
+
+		}
+		
 		else if (rb == K_ESC)
 			//ft_exit();
 			ft_exit();
@@ -112,8 +165,8 @@ int	main(void)
 		return 1;
 	}
 
-	change_settings();
-
+	//raw_settings();
+	cbreak_settings();
 	read_loop();
 
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &saved);
