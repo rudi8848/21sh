703622c62a7024ffc360938b257b02575ef4bb92
rudi8848 <anna.v8848@gmail.com>
2018-09-02 15:37:31 +0300

    heredoc don't end

--- heredoc.c
+++ heredoc.c
@@ -6,12 +6,13 @@ int		ft_heredoc(char **argv, int infile, int outfile)
 
 	ft_bzero(line, MAXLINE);
 	read_more(line, 1, "heredoc> ");
-	ft_printf("%s");
+	ft_putstr_fd(line, outfile);
+	//ft_bzero(line, MAXLINE);
 	while(ft_strcmp(line, argv[1]))
 	{
 
 		read_more(line, 1, "heredoc> ");
-		ft_printf("%s");
+		ft_putstr_fd(line, outfile);
 		ft_bzero(line, MAXLINE);
 	}
 	return 0;
